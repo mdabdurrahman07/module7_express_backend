4c2fd72578eaab205e220c5dd336dc87c879a9e8
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-05-13 21:24:44 +0600

    Refactor server.ts for improved readability and structure

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,37 @@
-import express, { type Request, type Response } from 'express'
+import express, { type Request, type Response } from "express";
 
+const app = express();
+const port = 5500;
 
-const app = express()
-const port = 5500
+// middleware
 
-// middleware 
+app.use(express.json());
+app.use(express.text());
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
 
-app.use(express.json())
+app.get("/", (req: Request, res: Response) => {
+  res.send("This is new server");
+});
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('This is new server')
-})
-
-app.post('/', (req: Request, res: Response) =>{
-    console.log("this is body",req.body)
-})
+app.post("/", (req: Request, res: Response) => {
+  //   console.log("this is body", req.body);
+  const body = req.body;
+  const { name, email, role } = body;
+  res.status(201).json({
+    message: "Created",
+    data: {
+      name,
+      email,
+      role,
+    },
+    error: false,
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
